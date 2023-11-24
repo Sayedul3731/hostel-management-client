d28ebb2f76278e5bd317cd8b257f31eac812989c
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-24 17:58:29 +0600

    Footer section added

--- src/Pages/Home/MealsByCategory/MealCard/MealCard.jsx
+++ src/Pages/Home/MealsByCategory/MealCard/MealCard.jsx
@@ -10,7 +10,7 @@ const MealCard = ({meal}) => {
           <h2 className="card-title">{title}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Details</button>
           </div>
         </div>
       </div>
