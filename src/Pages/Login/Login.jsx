b3b6b79d52ed1e5d6f835fc552fd52f86a02eb40
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-24 22:26:52 +0600

    password hide and show functionality added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from "react-router-dom"
 import { AuthContext } from '../../Provider/AuthProvider/AuthProvider';
 import Swal from 'sweetalert2';
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
-    const {logIn} = useContext(AuthContext);
-    const onSubmit = (data) =>{
+    const [show, setShow] = useState(false)
+    const { logIn } = useContext(AuthContext);
+
+    const onSubmit = (data) => {
         logIn(data.email, data.password)
-        .then(result =>{
-            console.log(result.user);
-            if(result.user) {
-                Swal.fire({
-                    title: "Success!",
-                    text: "User Logged In Successfully.",
-                    icon: "success"
-                  });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        })
+            .then(result => {
+                console.log(result.user);
+                if (result.user) {
+                    Swal.fire({
+                        title: "Success!",
+                        text: "User Logged In Successfully.",
+                        icon: "success"
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     return (
@@ -33,9 +36,14 @@ const Login = () => {
                     <p>
                         <input className='w-full my-3 px-3 py-1' placeholder='Your Email' {...register('email', { required: true })} />
                     </p>
-                    <p>
-                        <input className='w-full my-3 px-3 py-1' placeholder='Your Password' {...register('password', { required: true })} />
-                    </p>
+                    <div className='relative'>
+                        <p>
+                            <input type={ show ? 'text': 'password'} className='w-full my-3 px-3 py-1' placeholder='Your Password' {...register('password', { required: true })} />
+                        </p>
+                        <p onClick={() => setShow(!show)} className=' absolute cursor-pointer -mt-[37px] ml-[414px]'>
+                            {show ? <BsFillEyeSlashFill></BsFillEyeSlashFill> : <BsFillEyeFill></BsFillEyeFill>}
+                        </p>
+                    </div>
                     <input className='text-center font-semibold text-white w-full mt-5 bg-red-500 py-2' type="submit" />
                 </form>
                 <p className='mt-6 text-white text-center'>Don't Have An Account? Please <Link to="/register"><span className='text-red-500 font-semibold '>Register</span></Link> </p>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { AuthContext } from '../../Provider/AuthProvider/AuthProvider';
 import Swal from 'sweetalert2';
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 
 const Register = () => {
 
-    const {userCreate} = useContext(AuthContext);
+    const { userCreate } = useContext(AuthContext);
+    const [show, setShow] = useState(false)
 
     const {
         register,
@@ -17,21 +19,21 @@ const Register = () => {
 
     const onSubmit = (data) => {
         userCreate(data.email, data.password)
-        .then(result => {
-            console.log(result.user);
-            if(result.user){
-                Swal.fire({
-                    title: "Success!",
-                    text: "User Created Successfully.",
-                    icon: "success"
-                  });
-            }
-        })
-        .catch(error => {
-            console.log(error.message);
-        })
+            .then(result => {
+                console.log(result.user);
+                if (result.user) {
+                    Swal.fire({
+                        title: "Success!",
+                        text: "User Created Successfully.",
+                        icon: "success"
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
     }
-  
+
 
     return (
         <div className='min-h-[600px] w-full mx-auto m-3 flex justify-center items-center border'>
@@ -39,29 +41,34 @@ const Register = () => {
                 <h1 className='text-2xl font-semibold text-white mb-8'>Please Register!</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <p>
-                        <input className='w-full my-4 px-3 py-1' placeholder='Your Name' {...register('name',{ required: true })} />
+                        <input className='w-full my-4 px-3 py-1' placeholder='Your Name' {...register('name', { required: true })} />
                     </p>
                     <p>
-                        <input className='w-full my-4 px-3 py-1' placeholder='Your Email' {...register('email',{ required: true })} />
+                        <input className='w-full my-4 px-3 py-1' placeholder='Your Email' {...register('email', { required: true })} />
                     </p>
-                    <p>
-                        <input type='password' className='w-full my-4 px-3 py-1' placeholder='Your Password' {...register('password', {
-                            required: true,
-                            minLength: 6,
-                            maxLength: 20,
-                            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{6}/
+                    <div className='relative'>
+                        <p>
+                            <input type={show ? 'text' : 'password'} className='w-full my-4 px-3 py-1' placeholder='Your Password' {...register('password', {
+                                required: true,
+                                minLength: 6,
+                                maxLength: 20,
+                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{6}/
                             })} />
-                    </p>
-                    {errors.password?.type === 'required' && <p className='text-red-600'>Password is required</p> }
-                    {
-                        errors.password?.type === 'minLength' && <p className='text-red-600'>Password must be 6 characters</p> 
-                    }
-                    {
-                        errors.password?.type === 'maxLength' && <p className='text-red-600'>Password must be less than 20 characters</p> 
-                    }
-                    {
-                        errors.password?.type === 'pattern' && <p className='text-red-600'>Password must have one capital letter, one special character, one digit and one small letter</p> 
-                    }
+                        </p>
+                        {errors.password?.type === 'required' && <p className='text-red-600'>Password is required</p>}
+                        {
+                            errors.password?.type === 'minLength' && <p className='text-red-600'>Password must be 6 characters</p>
+                        }
+                        {
+                            errors.password?.type === 'maxLength' && <p className='text-red-600'>Password must be less than 20 characters</p>
+                        }
+                        {
+                            errors.password?.type === 'pattern' && <p className='text-red-600'>Password must have one capital letter, one special character, one digit and one small letter</p>
+                        }
+                        <p onClick={() => setShow(!show)} className='border absolute cursor-pointer -mt-10 ml-[414px]'>
+                            { show ? <BsFillEyeSlashFill></BsFillEyeSlashFill> :<BsFillEyeFill></BsFillEyeFill>}
+                        </p>
+                    </div>
                     <input className='text-center font-semibold text-white w-full mt-5 bg-red-500 py-2' type="submit" />
                 </form>
                 <p className='mt-6 text-white text-center'>Already Have An Account? Please <Link to="/login"><span className='text-red-500 font-semibold '>Login</span></Link> </p>
