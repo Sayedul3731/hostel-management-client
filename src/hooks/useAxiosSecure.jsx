62dc42f9c25041629999b57859348819f95a1862
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-28 17:11:03 +0600

    use interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,20 @@ const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000/'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        console.log('request stopped by interceptors', token)
+        config.headers.authorization = `Bearer ${token}`
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    });
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        console.log('status error in interceptor', error);
+        return Promise.reject(error);
+    });
     return axiosSecure;
 };
 
