2c2ec9e75fdd0bf7a4549c1af079e0694335e93f
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-30 22:32:02 +0600

    review update functionality added

--- src/Pages/Dashboard/MyReviews/MyReviews.jsx
+++ src/Pages/Dashboard/MyReviews/MyReviews.jsx
@@ -2,17 +2,21 @@ import Swal from "sweetalert2";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
 
 
 const MyReviews = () => {
     const user = useAuth();
     const axiosSecure = useAxiosSecure();
     const [meals, setMeals] = useState([]);
+    const { control, handleSubmit, setValue } = useForm();
+    const [updateMeal, setUpdateMeal] = useState({})
 
     const [currentPage, setCurrentPage] = useState(0);
-    const [paginateReviewsMeals, setPaginateReviewsMeals] = useState([])
+    const [paginateReviewsMeals, setPaginateReviewsMeals] = useState([]);
+    const [userReview, setUserReview] = useState({})
 
     const totalData = meals.length;
     const itemPerPage = 10;
@@ -55,13 +59,42 @@ const MyReviews = () => {
             }
         });
     }
+
+    React.useEffect(() => {
+        setValue('reviews', userReview.review);
+      }, [setValue, userReview]);
+
     const handleUpdate = (meal) => {
-        const newInfo = {
-            review: meal?.review
+        // const newInfo = {
+        //     review: meal?.review
+        // }
+        console.log('meal id', meal?._id);
+        setUpdateMeal(meal)
+        document.getElementById('update_modal').showModal()
+
+        axiosSecure.get(`/reviews/review/${meal?._id}`)
+            .then(res => {
+                console.log('get result', res.data.review);
+                setUserReview(res.data)
+            });
+    }
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        console.log('update Meal', updateMeal);
+        const newReview = {
+            review: data.reviews
         }
-        axiosSecure.patch(`/reviews/${meal._id}`, newInfo)
+        await axiosSecure.patch(`/reviews/review/${updateMeal?._id}`, newReview)
             .then(res => {
                 console.log(res.data);
+                if (res.data) {
+                    Swal.fire({
+                        title: "Success!",
+                        text: "Your Review Updated Successfully.",
+                        icon: "success"
+                    });
+                }
             })
     }
 
@@ -90,6 +123,7 @@ const MyReviews = () => {
             setCurrentPage(currentPage + 1)
         }
     }
+    console.log('review...-...', userReview.review);
     return (
         <div className="md:p-8">
             <SectionTitle heading='my reviews'></SectionTitle>
@@ -135,6 +169,28 @@ const MyReviews = () => {
                 }
                 <button onClick={handleNextPage}>Next</button>
             </div>
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <dialog id="update_modal" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="">
+                            <Controller
+                                name="reviews"
+                                control={control}
+                                render={({ field }) => <textarea {...field} style={{ width: "100%", minHeight: 100, padding: 5, backgroundColor: '#EFF3F7' }} />}
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-red-500 py-2 text-white font-semibold rounded-sm">Update Review</button>
+                    </form>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-sm btn-circle btn-ghost absolute right-0 top-0">✕</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
