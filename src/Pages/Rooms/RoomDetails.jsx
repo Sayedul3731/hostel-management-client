6b8fda85f3d39baf729a8ea1d32b83b19dc39d16
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-12 16:30:15 +0600

    check user badge when a user want to book any seat

--- src/Pages/Rooms/RoomDetails.jsx
+++ src/Pages/Rooms/RoomDetails.jsx
@@ -2,42 +2,61 @@ import { useLoaderData } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
+import useUsers from "../../hooks/useUsers";
+import { useEffect, useState } from "react";
 
 
 
 const RoomDetails = () => {
     const user = useAuth();
+    const [users] = useUsers();
+    const [currentUser, setCurrentUser] = useState({})
+    console.log(users);
     const axiosSecure = useAxiosSecure()
     console.log(user);
     const data = useLoaderData();
-    console.log(data);
+
+    useEffect(() => {
+        const filteredUser = users.filter(eachUser => eachUser?.email === user?.email)
+        setCurrentUser(filteredUser[0])
+        console.log(currentUser);
+    }, [currentUser, users, user?.email])
+
     const handleBooking = (seat) => {
         console.log(seat);
         const newBookingInfo = {
             room_number: data?.room_number,
-            seat_no : seat?.seat_no,
+            seat_no: seat?.seat_no,
             studentsEmail: user?.email
         }
-        axiosSecure.post(`/bookedSeats`, newBookingInfo)
-        .then(res => {
-            console.log('booking successfully',res.data)
-            if(res?.data){
-                Swal.fire({
-                    title: "Good job!",
-                    text: "You Booked the room!",
-                    icon: "success"
-                  });
-                const updateInfo ={
-                    students_name: user?.displayName,
-                    students_email: user?.email,
-                    seat_no: seat?.seat_no
-                }
-                axiosSecure.patch(`/seats/${data?.room_number}`,updateInfo)
+        if (currentUser.Badge !== 'Bronze') {
+            axiosSecure.post(`/bookedSeats`, newBookingInfo)
                 .then(res => {
-                    console.log(res.data);
+                    console.log('booking successfully', res.data)
+                    if (res?.data) {
+                        Swal.fire({
+                            title: "Good job!",
+                            text: "You Booked the room!",
+                            icon: "success"
+                        });
+                        const updateInfo = {
+                            students_name: user?.displayName,
+                            students_email: user?.email,
+                            seat_no: seat?.seat_no
+                        }
+                        axiosSecure.patch(`/seats/${data?.room_number}`, updateInfo)
+                            .then(res => {
+                                console.log(res.data);
+                            })
+                    }
                 })
-            }
-        })
+        }else{
+            Swal.fire({
+                title: "Oh Sorry!",
+                text: "You aren't purchase any package!",
+                icon: "error"
+            });
+        }
     }
     return (
         <div>
@@ -56,9 +75,9 @@ const RoomDetails = () => {
                                 }
                             </div>
                             <div>
-                               {
-                                seat.status === 'occupied' ?  <button className="bg-red-600 px-3 py-2 text-white font-semibold rounded-sm w-full btn" disabled>Booking Now</button> :  <button onClick={() => handleBooking(seat)} className="bg-red-600 px-3 py-2 text-white font-semibold rounded-sm w-full btn">Booking Now</button>
-                               }
+                                {
+                                    seat.status === 'occupied' ? <button className="bg-red-600 px-3 py-2 text-white font-semibold rounded-sm w-full btn" disabled>Booking Now</button> : <button onClick={() => handleBooking(seat)} className="bg-red-600 px-3 py-2 text-white font-semibold rounded-sm w-full btn">Booking Now</button>
+                                }
                             </div>
                         </div>
                     </div>)
