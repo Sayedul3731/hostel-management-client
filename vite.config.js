ce6e7dfa1fc1ca1de571ec88f6edd46a25e96323
MD SAYEDUL HAQUE <mdsayedulhaque932@gmail.com>
2024-12-19 00:14:09 +0600

    add: some modify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Manual chunking to split large dependencies into separate chunks
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    // Adjust chunk size warning limit
+    chunkSizeWarningLimit: 1000, // Increase to 1000kB
+  },
+});
